aa0069f3e7f1330ca24c60b77ce283cb5de06ddd
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-23 22:09:47 -0400

    working accommodations api

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const learningDisabilities = {
+    'adhd': {
+        'disability': 'ADHD',
+        'explanation': 'n/a',
+        'accommodation': 'Fidget Toys'
+    },    
+    'dyslexia': {
+        'disability': 'Dyslexia',
+        'explanation': 'n/a',
+        'accommodation': 'Unknown'
+    },
+    'dyscalculia': {
+        'disability': 'Dyscalculia',     
+        'explanation': 'n/a',
+        'accommodation': 'Unknown'
+    },
+    'unknown': {
+        'disability': 'Unknown',     
+        'explanation': 'Unknown',
+        'accommodation': 'Unknown'
+    }
+}
+
+//looks very similar to an event listener in js but it is network request instead of click
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const disabilityName = request.params.name.toLowerCase()
+    if(learningDisabilities[disabilityName]) {
+        response.json(learningDisabilities[disabilityName])
+    } else {
+        response.json(learningDisabilities['unknown'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}.`)
+})
